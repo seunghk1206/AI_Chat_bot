62145cf6f485bb74cb033ba2b819d098c9a97e27
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-04-28 01:01:20 +0900

    Update AI_Chat_bot.py

--- AI_Chat_bot.py
+++ AI_Chat_bot.py
@@ -1,11 +1,10 @@
 import nltk
-#nltk.download('punkt')
 from nltk.stem.lancaster import LancasterStemmer
 stemmer = LancasterStemmer()
 
 import numpy
-# import tflearn
-# import tensorflow
+import tflearn
+import tensorflow
 import random
 import json
 
@@ -15,16 +14,47 @@ with open("intents.json") as file:
 
 words = []
 labels = []
-docs = []
+docs_x = []
+docs_y = []
 
 for intent in data["intents"]:
     for pattern in intent["patterns"]:
         wrds = nltk.word_tokenize(pattern)
         words.extend(wrds)
-        docs.append(pattern)
+        docs_x.append(pattern)
+        docs_y.append(intent["tag"])
     
     if intent["tag"] not in labels:
         labels.append(intent["tag"])
 
-while True:
-    pass
+words = [stemmer.stem(w.lower()) for w in words if w != '?']
+
+words = sorted(list(set(words))) # set 은 반복되는 단어만 선택해서 리스트 형성
+
+labels = sorted(labels)
+
+training = []
+output = []
+
+out_empty = [0 for _ in range(len(labels))]
+
+for x, doc in enumerate(docs_x): # enumerate => 같은 리스트지만 x에는 index -> iterator
+    bag = []
+    wrds = [stemmer.stem(w.lower) for w in doc] # 패턴이 lower case 된것
+    for w in words:
+        if w in wrds:
+            bag.append(1)
+        else:
+            bag.append(0)
+
+    output_row = out_empty[:]
+    output_row[labels.index(docs_y[x])] = 1
+
+    training.append(bag)
+    output.append(output_row)
+
+training = numpy.array(training)
+ouput = numpy.array(output)
+
+# brute force
+# greedy algorithm
